e5779993876dc7b771e494798bf16fa596b13952
lanlanlanlan704 <2115675369@qq.com>
2026-03-02 23:18:28 +0800

    完成角色创建

---
+++ frontend/src/views/create/character/components/Profile.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const props = defineProps(['profile'])
+const myProfile = ref(props.profile)
+watch(() => props.profile, newVal => {
+  myProfile.value = newVal
+})
+
+
+defineExpose({
+  myProfile,
+})
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <label class="label text-base">角色介绍</label>
+    <textarea v-model="myProfile" class="textarea w-108" rows="6"></textarea>
+  </fieldset>
+</template>
+
+<style scoped>
+
+</style>
