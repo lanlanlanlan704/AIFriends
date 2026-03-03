bde342ff42a3a821999f9e66f641316c9fc80d83
lanlanlanlan704 <2115675369@qq.com>
2026-03-03 23:22:47 +0800

    实现好友列表

--- frontend/src/components/character/Character.vue
+++ frontend/src/components/character/Character.vue
@@ -1,14 +1,17 @@
 <script setup>
-import {ref} from "vue";
+import {ref, useTemplateRef} from "vue";
 import {useUserStore} from "@/stores/user.js";
 import UpdateIcon from "@/components/character/icons/UpdateIcon.vue";
 import RemoveIcon from "@/components/character/icons/RemoveIcon.vue";
 import api from "@/js/http/api.js";
+import ChatField from "@/components/character/chat_field/ChatField.vue";
+import {useRouter} from "vue-router";
 
-const props = defineProps(['character', 'canEdit'])
+const props = defineProps(['character', 'canEdit', 'canRemoveFriend', 'friendId'])
 const emit = defineEmits(['remove'])
 const isHover = ref(false)
 const user = useUserStore()
+const router = useRouter()
 
 async function handleRemoveCharacter() {
   try {
@@ -21,23 +24,62 @@ async function handleRemoveCharacter() {
   } catch (err) {
   }
 }
+
+async function handleRemoveFriend() {
+  try {
+    const res = await api.post('/api/friend/remove/', {
+      friend_id: props.friendId,
+    })
+    if (res.data.result === 'success') {
+      emit('remove', props.friendId)
+    }
+  } catch (err) {
+  }
+}
+
+const chatFieldRef = useTemplateRef('chat-field-ref')
+const friend = ref(null)
+
+async function openChatField() {
+  if (!user.isLogin()) {
+    await router.push({
+      name: 'Login'
+    })
+  } else {
+    try {
+      const res = await api.post('/api/friend/get_or_create/', {
+        character_id: props.character.id,
+      })
+      const data = res.data
+      if (data.result === 'success') {
+        friend.value = data.friend
+        chatFieldRef.value.showModal()
+      }
+    } catch (err) {
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <div class="avatar cursor-pointer" @mouseover="isHover=true" @mouseout="isHover=false">
+    <div class="avatar cursor-pointer" @mouseover="isHover=true" @mouseout="isHover=false" @click="openChatField">
       <div class="w-60 h-100 rounded-2xl relative">
-        <img :src="character.background_image" class="transition-transform duration-300" :class="{'scale-120': isHover}"
-             alt="">
+        <img :src="character.background_image" class="transition-transform duration-300" :class="{'scale-120': isHover}" alt="">
         <div class="absolute left-0 top-50 w-60 h-50 bg-linear-to-t from-black/40 to-transparent"></div>
 
         <div v-if="canEdit && character.author.user_id === user.id" class="absolute right-0 top-50">
-          <RouterLink :to="{name: 'update-character', params: {character_id: character.id}}"
-                      class="btn btn-circle btn-ghost bg-transparent">
-            <UpdateIcon/>
+          <RouterLink @click.stop :to="{name: 'update-character', params: {character_id: character.id}}" class="btn btn-circle btn-ghost bg-transparent">
+            <UpdateIcon />
           </RouterLink>
-          <button @click="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
-            <RemoveIcon/>
+          <button @click.stop="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
+            <RemoveIcon />
+          </button>
+        </div>
+
+        <div v-if="canRemoveFriend" class="absolute right-0 top-50">
+          <button @click.stop="handleRemoveFriend" class="btn btn-circle btn-ghost bg-transparent">
+            <RemoveIcon />
           </button>
         </div>
 
@@ -54,8 +96,7 @@ async function handleRemoveCharacter() {
         </div>
       </div>
     </div>
-    <RouterLink :to="{name: 'Space', params: {user_id: character.author.user_id}}"
-                class="flex items-center mt-4 gap-2 w-60">
+    <RouterLink :to="{name: 'Space', params: {user_id: character.author.user_id}}" class="flex items-center mt-4 gap-2 w-60">
       <div class="avatar">
         <div class="w-7 rounded-full">
           <img :src="character.author.photo" alt="">
@@ -63,6 +104,7 @@ async function handleRemoveCharacter() {
       </div>
       <div class="text-sm line-clamp-1 break-all">{{ character.author.username }}</div>
     </RouterLink>
+    <ChatField ref="chat-field-ref" :friend="friend" />
   </div>
 </template>
 
--- frontend/src/views/create/character/UpdateCharacter.vue
+++ frontend/src/views/create/character/UpdateCharacter.vue
@@ -60,7 +60,7 @@ async function handleUpdate(){
     formData.append('name',name)
     formData.append('profile', profile)
     if (backgroundImage!==character.value.background_image){
-      formData.append('background_image', base64ToFile(photo,'background_image.png'))
+      formData.append('background_image', base64ToFile(backgroundImage,'background_image.png'))
     }
     if (photo !== character.value.photo){
       formData.append('photo', base64ToFile(photo,'photo.png'))
